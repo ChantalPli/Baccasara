ed494de01952649a9fa1ed9860bec23e15c6a6dc
Chantal <chantal.pilia01@gmail.com>
2025-03-24 10:05:15 -0500

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.pdf', '**/*.jpg', '**/*.JPG', '**/*.jpeg', '**/*.JPEG', '**/*.png', '**/*.PNG'],
   server: {
     cors: true
-  }
+  },
+  base: '/Baccasara/'
 })
